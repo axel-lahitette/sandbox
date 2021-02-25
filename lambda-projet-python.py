eb539973e5e4923fd473262e479ca944cd02366a
Axel Lahitette <a.lahitette@es-be.net>
2021-02-25 18:50:16 +0100

    update

--- lambda-projet-python.py
+++ lambda-projet-python.py
@@ -47,37 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# MENU= int(input("Saisir 1 pour entrer un nouveau contact, 2 pour effectuer une recherche et 0 pour quitter"))
-# MENU=1
-# while MENU!=0:
-#     if MENU==1:
-#         numero=input("Entrez un numéro ")
-#         monFichier=open('fichier.txt','a')
-#         monFichier.write("Le numéro : ")
-#         monFichier.write(numero)
-#         monFichier.write("\n")
-#         monFichier.close()
-#         print('Numéro Enregistré')
-#
-#         nom=input("Entrer le nom correspondant : ")
-#         monFichier=open('fichier.txt','a')
-#         monFichier.write("Nom : ")
-#         monFichier.write(nom)
-#         monFichier.write("\n")
-#         monFichier.close()
-#         print('Le nom a bien était enregistré')
-#         MENU=int(input("Saisir 1 pour entrer un nouveau contact, 2 pour effectuer une recherche et 0 pour quitter"))
-#
-#     if MENU==2:
-#         monFichier=open('fichier.txt','r')
-        # for ligne in monFichier:
-        #     if recherche in ligne:
-        #         print(ligne)
-        #         break
-        # monFichier.close()
-#         print(numero)
-#         MENU=int(input("Saisir 1 pour entrer un nouveau contact, 2 pour effectuer une recherche et 0 pour quitter"))
-#
-
