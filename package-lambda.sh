e0df2ccf047e8f3effb29f1125b71de879135088
Guillaume Lahitette <guillaume@lahitette.net>
2021-02-25 23:51:20 +0100

    Commit

--- package-lambda.sh
+++ package-lambda.sh
@@ -1,2 +1,2 @@
-# aws cloudformation package --template ./lambda-cf.yml --s3-bucket mybucket --output yaml --profile gl-gekko  > packaged-lambda-template.yml
-aws-vault exec gl-gekko -- aws cloudformation package --template ./lambda-cf.yml --s3-bucket mybucket --output yaml > packaged-lambda-template.yml
+aws-vault exec gl-gekko -- aws cloudformation package --template ./lambda-cf.yml --s3-bucket axel-project-bucket --output yaml > packaged-lambda-template.yml
+aws-vault exec gl-gekko -- aws s3 ls axel-project-bucket
