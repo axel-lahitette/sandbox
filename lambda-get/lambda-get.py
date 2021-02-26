24fe7ac80224ad1aa230ca4b184fc3ecba4c3e71
Guillaume Lahitette <guillaume@lahitette.net>
2021-02-26 10:12:32 +0100

    Commit for Axel's project

--- lambda-get/lambda-get.py
+++ lambda-get/lambda-get.py
@@ -4,12 +4,22 @@
 Created on Wed Feb 24 15:55:25 2021
 
 @author: axel
+
+Return the number of the person using the name provided as argument
 """
 
 import json
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
+    # myFile = open('contacts.txt','r')
+    # for ligne in myFile:
+    #     if name in ligne:
+    #         number = ligne
+    #         print(ligne)
+    #         break
+    # myFile.close()
+    # return number
     return {
         'statusCode': 200,
         'headers': {
@@ -17,46 +27,3 @@ def handler(event, context):
         },
         'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
     }
-
-
-"""
-Return the number of the person using the name provided as argument
-"""
-def get(name):
-    myFile = open('contacts.txt','r')
-    for ligne in myFile:
-        if name in ligne:
-            number = ligne
-            print(ligne)
-            break
-    myFile.close()
-    return number
-
-"""
-Insert the contact of the person using the name and number provided as arguments
-"""
-def put(name, number):
-    myFile = open('contacts.txt','a')
-    myFile.write("Number : ")
-    myFile.write(number)
-    myFile.write("\t")
-    myFile.write("Name : ")
-    myFile.write(name)
-    myFile.write("\n")
-    myFile.close()
-
-"""
-Delete the contact of the person with the name provided as argument
-"""
-def delete(name):
-    return
-
-def main():
-    exemple_nom = "Guillaume"
-    exemple_num = "0644093517"
-    print(put(exemple_nom, exemple_num))
-    print(get(exemple_nom))
-    print(delete(exemple_nom))
-
-if __name__ == "__main__":
-    main()
