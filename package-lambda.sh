63085e8423f3ad1b8471bfe9c4bc6b9a88646600
Guillaume Lahitette <guillaume@lahitette.net>
2021-02-26 09:38:34 +0100

    Commit for Axel's project

--- package-lambda.sh
+++ package-lambda.sh
@@ -1,2 +1,3 @@
 aws-vault exec gl-gekko -- aws cloudformation package --template ./lambda-cf.yml --s3-bucket axel-project-bucket --output yaml > packaged-lambda-template.yml
+aws-vault exec gl-gekko -- aws s3 ls
 aws-vault exec gl-gekko -- aws s3 ls axel-project-bucket
